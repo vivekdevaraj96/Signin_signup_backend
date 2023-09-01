6d995140a61209ca0fd74e2eeb80c5a38ed110d2
vivekdevaraj96 <vivekdeva96@gmail.com>
2023-09-01 11:01:11 +0530

    'forget_password'

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 const cors=require('cors')
 
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const {dburl}=require('../common/dbconfig')
 mongoose.connect(dburl)
 let jwt=require('jsonwebtoken')
 var JWT_secret="hxfawbiufvimaboiga9wfewfpwefhrpohrawn90fh"
+const nodemailer = require("nodemailer");
 
 
 /* GET home page. */
@@ -19,7 +20,7 @@ router.post('/signup',async(req, res)=>{
     try {
         let {fname,lname, email, password}=req.body
 
-        let encryptedPassword=await bcrypt.hash(password,10)
+        var encryptedPassword=await bcrypt.hash(password,10)
 
         let user= await userModel.findOne({email: email})
         if (!user) {
@@ -82,17 +83,50 @@ router.post('/forgetpassword',async(req,res)=>{
     if(!olduser){
       return res.json({message:"user Doesn't exists"})
     }
-    let randomstring=bcrypt.genSalt(10)
+    let randomstring=await bcrypt.genSalt(10)
     const secret=JWT_secret+olduser.password;
     olduser.randomstring=randomstring;
     olduser.save()
-    const token=jwt.sign({email:olduser.email,id:olduser.id,randomstring:randomstring}, secret, {
+    const token=await jwt.sign({email:olduser.email,id:olduser.id,randomstring:randomstring}, secret, {
       expiresIn:"5m"
     })
-    const link=`http://localhost:8000/reset-password/${olduser.id}/${token}`
+    const link=`https://signin-signup-backend-cpk7.onrender.com/secretkey/${olduser.id}/${token}`
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'vivekdevaraj603@gmail.com',
+        pass: 'dwnqccjpguxbiskw'
+      }
+    });
+    
+    var mailOptions = {
+      from: 'vivekdevaraj603@gmail.com',
+      to: email,
+      subject: 'Password Reset',
+      text: link
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
     console.log(link)
-    res.status(200).send({message:"link send to mail"})
+    res.status(200).send({message:"link send to mail",id:olduser.id,key:token})
 
+  } catch (error) {
+    res.status(400).send({message:error})
+  }
+})
+
+router.get('/secretkey/:id/:token',async(req,res)=>{
+  const {id,token}=req.params;
+  try {
+    let data=jwt.decode(token)
+    console.log(`data .....${data}`)
+    res.status(200).send(`secret key =      ${data.randomstring}`)
   } catch (error) {
     res.status(400).send({message:error})
   }
@@ -100,20 +134,22 @@ router.post('/forgetpassword',async(req,res)=>{
 
 router.post('/reset-password/:id/:token',async(req,res)=>{
   const {id,token}=req.params;
+  const {password, secretkey}=req.body;
   try {
-    
-    
-    
-    console.log(`id...... ${id}`)
-    console.log(`token ......${token}`)
-    
+    console.log(id)
+    let data=jwt.decode(token)
+    console.log(`data .....${data}`)
+   
     const olduser=await userModel.findOne({_id:id});
-    console.log(olduser)
     if(olduser){
-    const secret=JWT_SECRET+olduser.password;
-    const verify=await jwt.verify(token,secret);
-      console.log(verify)
-      res.send(verify)
+      if (olduser.randomstring==secretkey) {
+        var encryptedPassword=await bcrypt.hash(password,10)
+        olduser.password=encryptedPassword;
+        olduser.save();
+        res.status(200).send({message:"Password Changed",status:"ok", data})
+      } else {
+        res.status(400).send({message:"error in random string", data})
+      }     
     }else{
       res.status(400).send({message:"Unable to retrieve user data"})
     }
